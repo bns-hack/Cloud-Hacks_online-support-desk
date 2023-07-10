552ce09c7a45f476390ebbd0c3f9ce2c468a127e
Afzal <94980910+afzalbin64@users.noreply.github.com>
2023-07-10 20:24:22 +0000

    updates docker env

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const YOUR_REACT_BACK_APIBASE_URL='http://localhost:5000'
-const API_URL = YOUR_REACT_BACK_APIBASE_URL + '/api/users/'
+const REACT_BACK_API_URL = process.env.REACT_BACK_API_URL
+const API_URL = REACT_BACK_API_URL + '/api/users/'
 
 // Register user
 const register = async (userData) => {
